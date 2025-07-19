5fab6c681458c008c31b305aee9e1ee2b3e1828f
leena-gouda <L.Gouda01794@student.aast.edu>
2025-07-19 16:23:59 +0300

    refactored authentication cubit and views, firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'core/routing/app_router.dart';
 import 'core/routing/routes.dart';
 import 'core/utils/notification_service.dart';
+import 'firebase_options.dart';
+
 
 bool isLogin = false;
 
